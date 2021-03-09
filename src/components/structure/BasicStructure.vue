124c0d7d96d5efef9aef95f614c3e787bb6a96ec
WeiLi <lw@blask.cn>
2021-03-09 23:42:17 +0800

    提交物业费用前端模块

---
+++ src/components/structure/BasicStructure.vue
@@ -0,0 +1,29 @@
+<template>
+  <el-container style="height:100%">
+    <el-header>Title</el-header>
+    <el-main></el-main>
+    <el-footer>Footer</el-footer>
+  </el-container>
+</template>
+
+<script>
+export default {
+  name: "BasicStructure",
+}
+</script>
+
+<style scoped>
+.el-header, .el-footer {
+  background-color: #B3C0D1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+  height: 100%;
+}
+
+.el-main {
+  background-color: #E9EEF3;
+  color: #333;
+  text-align: center;
+}
+</style>
