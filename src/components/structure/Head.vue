fe689f167e665eb58752922f27100b3bfb800a77
WeiLi <lw@blask.cn>
2021-03-09 23:41:37 +0800

    Revert "提交物业费用前端模块"
    This reverts commit 04ec2335

---
+++ src/components/structure/Head.vue
@@ -0,0 +1,24 @@
+<template>
+  <el-container style="height:10%">
+    <el-header>Title</el-header>
+  </el-container>
+  <router-view></router-view>
+</template>
+
+<script>
+export default {
+name: "Head"
+}
+</script>
+
+<style scoped>
+
+.el-header{
+  background-color: #B3C0D1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+  float: top;
+}
+
+</style>
